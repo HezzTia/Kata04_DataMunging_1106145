8a5803910e70478cb815ed64a7518d30a4886e9f
HezzTia <emilycabreraarache@gmail.com>
2024-02-18 09:40:00 -0400

    advance in first program

--- Program.fs
+++ Program.fs
@@ -15,7 +15,16 @@ type WeatherInfo = { dayNumber : int; maxTemp : float; minTemp : float }
 let isNullOrWhiteSpace (s: string) = //string -> bool
     System.String.IsNullOrEmpty(s) || s.Trim().Length = 0
 
-let parseline (line : string) = 
-    let parts : string list = Regex.Split(line, @"\s+")  //string array
+let parseLine (line: string) =
+    let parts = Regex.Split(line, @"\s+")
                  |> Array.filter (fun s -> not (isNullOrWhiteSpace s))
                  |> Array.toList
+    if parts.Length < 14 then failwith message = $"Invalid data format: {line}"
+    try
+        let dayNumber = int parts.[0]
+        let maxTemp = float parts.[1]
+
+        let minTempwithAsterisk: string = parts.[2]
+        let minTemp: float = 
+            match Regex.Match(input = minTempwithAsterisk, pattern = @"(\d+\.\d*)\*?").Groups.[1].Value with
+            | "" -> failwith message = $"Error parsing MinT field: {line}"
