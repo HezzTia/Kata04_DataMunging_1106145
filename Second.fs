f8d49b3df43b20426e3d75b7d777f8595d50b45a
HezzTia <emilycabreraarache@gmail.com>
2024-02-18 10:10:07 -0400

    helper function done, kinda

--- Second.fs
+++ Second.fs
@@ -8,3 +8,20 @@ type TeamInfo = {
     GoalsFor: int;
     GoalsAgainst: int;
 }
+
+// Helper fuunction to parse a line and extract relevant information
+
+let parseline (line: string) = //string -> option<TeamInfo>
+    let parts : string array = line.Split(separator = [| ' '; '\t' |], options = System.StringSplitOptions.RemoveEmptyEntries)
+
+    // Ignore lines that don't contain team data
+    if parts.Length < 8 then None
+    else
+        let team = parts.[1]
+        let goalsFor = int parts.[6]
+        let goalsAgainst = int parts.[8]
+        Some { Team = team; GoalsFor = goalsFor; GoalsAgainst = goalsAgainst }  
+
+// Function to calculate the goal difference
+
+let calculateGoalDifference (team: )
