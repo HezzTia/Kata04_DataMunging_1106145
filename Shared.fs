6761ae0cf683750f5fe3d9ca9eb2e0da5ff870af
HezzTia <emilycabreraarache@gmail.com>
2024-02-18 10:25:30 -0400

    third part of the exercise?

--- Shared.fs
+++ Shared.fs
@@ -1,2 +1,23 @@
 module Shared
 
+// Data type for weather day information
+
+type DayData = {
+    DayNumber : int;
+    MaxTemp : float;
+    MinTemp : float;
+}
+
+open System.IO
+
+type Parser<' T> = {
+    Parseline: string -> Option<'T>
+}
+
+// Function to read data from a file using a provided parser
+
+let readData<'T> (filePath: string) (parser: Parser<'T>) =
+    File.ReadAllLines( filePath) 
+    |> Seq.skip 1 
+    |> List.ofSeq
+    |> List.choose parser.Parseline
